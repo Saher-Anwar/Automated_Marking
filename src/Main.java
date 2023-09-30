51b1843ad083b9c2999c96f3229b49082313a6e9
Saher Anwar <saher.ziauddin@dal.ca>
2023-09-30 12:21:50 -0300

    - Added Documents folder

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String currentDirectory = System.getProperty("user.dir");
         currentDirectory += "\\Documents";
+
         System.out.println("Current Directory: " + currentDirectory);
     }
 }
