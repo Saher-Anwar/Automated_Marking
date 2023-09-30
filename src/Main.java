71f04433427aeb3a39d0f0ac1b549ec387386c6c
Saher Anwar <saher.ziauddin@dal.ca>
2023-09-30 12:51:44 -0300

    - OS independent

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
+import org.cef.OS;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String currentDirectory = System.getProperty("user.dir");
-        String destinationDir = currentDirectory + "\\Documents";
-        String zippedDir = currentDirectory + "\\ZippedFiles";
+        String destinationDir, zippedDir;
 
-        Utilities.UnZip(zippedDir, currentDirectory);
-        System.out.println("Current Directory: " + currentDirectory);
+        if(OS.isMacintosh()){
+            destinationDir = currentDirectory + "/Documents";
+            zippedDir = currentDirectory + "/ZippedFiles/png2jpg.zip";
+        } else{
+            destinationDir = currentDirectory + "\\Documents";
+            zippedDir = currentDirectory + "\\ZippedFiles\\png2jpg.zip";
+        }
+
+        Utilities.UnZip(zippedDir, destinationDir);
+        System.out.println("Current Directory: " + destinationDir);
         System.out.println("Zipped Directory: " + zippedDir);
     }
 }
