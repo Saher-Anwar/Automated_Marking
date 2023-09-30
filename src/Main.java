f242f2323b3e0e083e6b3fb1e6db58b1134f1574
Saher Anwar <saher.ziauddin@dal.ca>
2023-09-30 12:31:09 -0300

    - Attempt to unzip files

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        String currentDirectory = System.getProperty("user.dir");
-        currentDirectory += "\\Documents";
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String currentDirectory = System.getProperty("user.dir");
+        String destinationDir = currentDirectory + "\\Documents";
+        String zippedDir = currentDirectory + "\\ZippedFiles";
+
+        Utilities.UnZip(zippedDir, currentDirectory);
         System.out.println("Current Directory: " + currentDirectory);
+        System.out.println("Zipped Directory: " + zippedDir);
     }
 }
